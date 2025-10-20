016f0a1cf9bf908f43257e58a30d804a8a6ae73f
Antonina <114867084+DulamaA@users.noreply.github.com>
2025-10-20 22:20:54 +0200

    Fix base URL format in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/DulamaA/The-last-todo",
+  base: "/https://github.com/DulamaA/The-last-todo/",
   plugins: [react(), tailwindcss()],
 });
